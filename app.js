912e0ebd3302f1c8ccf431e98532f94b8d03047c
Peter Kemley <peterkemley@hotmail.com>
2023-07-30 02:27:48 +0100

    Checkpoint In middle of fixing register post in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 // Set up the layout file
 app.set('layout', 'layout');
 app.use(expressLayouts);
@@ -34,6 +34,27 @@ app.get('/register', (req, res) => {
   res.render('register');
 });
 
+
+// POST request for user registration
+app.post('/register', (req, res) => {
+  console.log('Confirm POST REQUEST for Register');
+  const { name, email, password, confirmPassword } = req.body;
+
+  console.log('Received form data:');
+  console.log('Name:', name);
+  console.log('Email:', email);
+
+  // Check if the password and confirm password match
+  if (password !== confirmPassword) {
+    return res.status(400).send("Passwords do not match");
+  }
+
+  // Here, you can proceed with user registration logic (e.g., store user details in the database)
+  // For now, let's just send a response back to the client
+  const responseMessage = `User ${name} with email ${email} has been registered successfully!`;
+  res.send(responseMessage);
+});
+
 // About route
 app.get('/about', (req, res) => {
   console.log('Confirm GET REQUEST for About');
@@ -51,8 +72,6 @@ app.get('/contact', (req, res) => {
 an email through the contact form as this is a proof of concept the email does not actually send
 but a response is sent to the server to say thanks using the name variable from the form 
 FIXME: Fix submission message keep getting the undefined */
-app.use(express.urlencoded({ extended: true }));
-
 app.post('/contact', (req, res) => {
   console.log('Confirm POST REQUEST for Contact');
   const { name, email, message } = req.body;
