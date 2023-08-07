5921df187665c6c20afc9779b7dc47e4528c1a74
Peter Kemley <peterkemley@hotmail.com>
2023-08-07 01:21:41 +0100

    Added both APIs and Login but need to fix registration

--- app.js
+++ app.js
@@ -8,21 +8,38 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-// ./app.js
 const express = require('express');
 const session = require('express-session');
-const passport = require('./auth');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-const bodyParser = require('body-parser');
+const flash = require('express-flash')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+//As this application is a proof of concept the users will be stored locally.
+//In a real situation this would be a database such as MongoDB
+const users = [];
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 // Set up the layout file
 app.set('layout', 'layout');
@@ -40,17 +57,31 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-// Login route
-app.get('/login', (req, res) => {
-  console.log('Confirm GET REQUEST for Login');
-  res.render('login');
-});
+//IF userAuthenticated then render index and feed in Users name
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('home.ejs', { name: req.user.name })
+})
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/', // Redirect to the homepage upon successful login
-  failureRedirect: '/login', // Redirect back to the login page upon failed login
-  failureFlash: true // Enable flash messages for incorrect credentials
-}));
+// Login route
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  console.log('Confirm GET REQUEST for Login');
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.delete("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
 
 app.get('/logout', (req, res) => {
   req.logout();
@@ -58,39 +89,74 @@ app.get('/logout', (req, res) => {
 });
 
 // Register route
-app.get('/register', (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
   console.log('Confirm GET REQUEST for Register');
   res.render('register');
 });
 
-app.post('/register', async (req, res) => {
-  // ... (code to handle user registration and storing in the NeDB database)
-});
-
 // POST request for user registration
-app.post('/register', (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
   console.log('Confirm POST REQUEST for Register');
-  const { name, email, password, confirmPassword } = req.body;
-
-  console.log('Received form data:');
-  console.log('Name:', name);
-  console.log('Email:', email);
-
-  // Check if the password and confirm password match
-  if (password !== confirmPassword) {
-    return res.status(400).send("Passwords do not match");
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+    console.log(`User Registered Successfully`);
+  } catch {
+    console.log('User Registered Unsuccessfully please try again');
+    res.redirect('/register')
   }
+  console.log(users);
+})
 
-  // Here, you can proceed with user registration logic (e.g., store user details in the database)
-  // For now, let's just send a response back to the client
-  const responseMessage = `User ${name} with email ${email} has been registered successfully!`;
-  res.send(responseMessage);
-});
+  // const { password, confirmPassword } = req.body;
+  // // Check if the password and confirm password match
+  // if (password !== confirmPassword) {
+  //   return res.status(400).send("Passwords do not match");
+  // }
+  // // Here, you can proceed with user registration logic (e.g., store user details in the database)
+  // // For now, let's just send a response back to the client
+  // const responseMessage = `User ${name} with email ${email} has been registered successfully!`;
+  // res.send(responseMessage);
 
 // About route
 app.get('/about', (req, res) => {
   console.log('Confirm GET REQUEST for About');
   res.render('about');
+});
+
+// Scanner route
+app.get('/scanner', (req, res) => {
+  console.log('Confirm GET REQUEST for Scanner API');
+  res.render('scanner');
+});
+
+// Scanner POST
+app.post('/api/scanner', async (req, res) => {
+  const barcode = req.body.barcode; // Assuming the barcode is sent in the request body
+
+  // Make the API request to VeganCheck API using the Fetch API or Axios (you can choose either)
+  // For example, using the Fetch API
+  try {
+    const apiUrl = `https://api.vegancheck.me/v0/product/${barcode}`;
+    const response = await fetch(apiUrl, { method: "POST" });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data from VeganCheck API' });
+  }
+});
+
+// Route for Ingredients view
+app.get('/ingredient', (req, res) => {
+  console.log('Confirm GET REQUEST for Ingredients API');
+  res.render('ingredient');
 });
 
 // Contact route ----------------------------------ANYTHING BETWEEN THIS AND THE NEXT LONG ASS LINE IS TODO WITH CONTACT PAGE----------------------------------
@@ -120,6 +186,24 @@ app.post('/contact', (req, res) => {
   res.send(responseMessage);
 })
 // Contact route ----------------------------------THIS IS THE LONG ASS LINE I WAS REFERRING TO----------------------------------
+
+//checkAuthenticated and checkNotAuthenticated are two functions used to check if a user is logged in or not
+function checkAuthenticated(req, res, next) {
+  //IF USER IS AUTHENTICATED(LOGGED IN) THEN PROCEED ELSE REDIRECT TO LOGIN PAGE
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  //IF USER IS NOT AUTHENTICATED(LOGGED IN) THEN REDIRECT TO HOME ELSE PROCEED
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
 
 //IF RUNNING LOCALHOST PORT WILL BE 3000 but if hosted it will be the env variable AUTO SET BY HEROKU
 const PORT = process.env.PORT || 3000;
