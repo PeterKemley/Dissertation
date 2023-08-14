8a463c07bec09e73f233474fd921ecc5f1f94ea2
Peter Kemley <peterkemley@hotmail.com>
2023-08-14 16:49:00 +0100

    Updated TODO-List

--- TODO-List.js
+++ TODO-List.js
@@ -3,8 +3,6 @@
 Following this message is a TODO list of things that need done for the webapp
 
 TODO:Add logo and icon
-TODO:Add camera barcode scanner
-TODO:FIX API VIEWS
 TODO:FIX FORM WIDTHS
 
 Following this message is WANTS
